04b69ad403e0e2aa713cb5e77491e7e1a2fa0328
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2023-06-29 18:04:07 -0300

    Created the 3 screens

--- App.js
+++ App.js
@@ -1,14 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Workout" component={WorkoutScreen} />
+        <Stack.Screen name="HIIT" component={HIITScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Pressable onPress={() => navigation.navigate('Workout')}>
+        <Text>Go to Workout Screen</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate('HIIT')}>
+        <Text>Go to HIIT Screen</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+function WorkoutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>This is the Workout Screen</Text>
+      {/* Add workout screen content here */}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function HIITScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>This is the HIIT Screen</Text>
+      {/* Add HIIT screen content here */}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
 
 const styles = StyleSheet.create({
   container: {
