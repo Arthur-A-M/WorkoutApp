de547a9bee37243843787d4c833650f4994834c5
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2023-07-20 15:11:17 -0300

    Login screen improved
    Styled the login screen. Added a sign up pressable, without function yet. Implemented a remember me check box.

--- src/StackNavigation/Screens/Login/index.js
+++ src/StackNavigation/Screens/Login/index.js
@@ -1,52 +1,109 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Pressable } from 'react-native';
 
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { AntDesign } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
+
+import { storeObjectData, getObjectData } from '../../../Functions';
 
 import { styles } from './styles';
 
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [checkboxState, setCheckboxState] = useState(false);
+
+  const rememberKey = 'rememberWorkouApp';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const value = await getObjectData(rememberKey);
+      if (value !== null) {
+        setEmail(value.email);
+        setPassword(value.password);
+        setCheckboxState(true);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleLogin = () => {
     if (email.trim() !== '' && password.trim() !== '') {
+      const data = checkboxState ? { email, password } : null;
+      storeObjectData(rememberKey, data);
       navigation.navigate('Home', { email });
     }
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.inputView}>
-        <Feather name="mail" size={24} color="white" />
-        <TextInput
-          style={styles.textInput}
-          value={email}
-          onChangeText={setEmail}
-          placeholderTextColor="white"
-          placeholder="Email"
-          autoCapitalize="none"
-          autoCompleteType="email"
-          maxLength={50}
-        />
+      <View style={styles.inputsView}>
+        <View style={styles.inputView}>
+          <Feather name="mail" size={24} color="white" />
+          <TextInput
+            style={styles.textInput}
+            value={email}
+            onChangeText={setEmail}
+            placeholderTextColor="white"
+            placeholder="Email"
+            autoCapitalize="none"
+            autoCompleteType="email"
+            maxLength={50}
+          />
+        </View>
+        <View style={styles.inputView}>
+          <AntDesign name="lock" size={24} color="white" />
+          <TextInput
+            style={styles.textInput}
+            value={password}
+            onChangeText={setPassword}
+            placeholderTextColor="white"
+            placeholder="Password"
+            secureTextEntry={true}
+            autoCompleteType="password"
+            maxLength={27}
+          />
+        </View>
+        <View style={styles.checkboxView}>
+          <BouncyCheckbox
+            isChecked={checkboxState}
+            onPress={() => setCheckboxState(!checkboxState)}
+            fillColor="#e0fe10"
+            disableBuiltInState={true}
+          />
+          <Text style={styles.checkboxText}>Remember me</Text>
+        </View>
       </View>
-      <View style={styles.inputView}>
-        <AntDesign name="lock" size={24} color="white" />
-        <TextInput
-          style={styles.textInput}
-          value={password}
-          onChangeText={setPassword}
-          placeholderTextColor="white"
-          placeholder="Password"
-          secureTextEntry={true}
-          autoCompleteType="password"
-          maxLength={27}
-        />
+      <View style={styles.pressablesView}>
+        <Pressable
+          onPress={handleLogin}
+          style={({ pressed }) => [
+            styles.pressableLogin,
+            pressed && {
+              opacity: 0.7,
+              width: '65%',
+              height: 70,
+            },
+          ]}
+        >
+          <Text style={styles.textLogin}>Login</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => alert('Not implemented yet!')}
+          style={({ pressed }) => [
+            styles.pressableSignUp,
+            pressed && {
+              opacity: 0.7,
+              width: '45%',
+              height: 45,
+            },
+          ]}
+        >
+          <Text style={styles.textSignUp}>Sign Up</Text>
+        </Pressable>
       </View>
-      <Pressable onPress={handleLogin} style={styles.pressableLogin}>
-        <Text style={styles.textLogin}>Login</Text>
-      </Pressable>
     </View>
   );
 }
--- src/StackNavigation/Screens/Login/styles.js
+++ src/StackNavigation/Screens/Login/styles.js
@@ -5,19 +5,31 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#1c2227',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+  },
+  inputsView: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   },
   inputView: {
-   flexDirection: 'row', 
-   alignItems: 'center',
-   height: 50,
-   width: 360,
-   paddingHorizontal: 15,
-   borderColor: 'gray',
-   borderWidth: 1,
-   borderRadius: 25,
-   backgroundColor: '#2a2f37',
-   marginVertical: 6
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 50,
+    width: 360,
+    paddingHorizontal: 15,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 25,
+    backgroundColor: '#2a2f37',
+    marginVertical: 6
+  },
+  pressablesView: {
+    flex: 0.8,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
   },
   textInput: {
     color: 'white',
@@ -36,8 +48,33 @@ export const styles = StyleSheet.create({
     borderWidth: 1,
     backgroundColor: '#e0fe10',
   },
+  pressableSignUp: {
+    width: '50%',
+    height: 50,
+    marginVertical: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    borderColor: 'gray',
+    borderWidth: 1,
+    backgroundColor: '#2a2f37',
+  },
+  checkboxView: {
+    flexDirection: 'row',
+    width: '80%',
+    paddingVertical: 5
+  },
+  checkboxText: {
+    color: 'white',
+    fontSize: 18
+  },
   textLogin: {
     fontSize: 25,
     fontWeight: 'bold',
+  },
+  textSignUp: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
   }
 });
